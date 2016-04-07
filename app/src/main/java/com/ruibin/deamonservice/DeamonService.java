311e05e1da71062bce09a91a47d4df2dd8a481f6
Ruibin, Liu <lrb@pci-suntektech.com>
2016-04-07 18:27:42 +0800

    Initial commit.

---
+++ app/src/main/java/com/ruibin/deamonservice/DeamonService.java
@@ -0,0 +1,77 @@
+package com.ruibin.deamonservice;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+public class DeamonService extends Service {
+    public static void start(Context context) {
+        Intent i = new Intent(context, DeamonService.class);
+        i.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        context.startService(i);
+    }
+
+    public DeamonService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        L.i("DeamonService.onCreate");
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            startForeground(64324, new Notification());
+        } else {
+            startForeground(64324, new Notification());
+            startService(new Intent(this, InnerService.class));
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        L.i("DeamonService.onStartCommand, flags=" + flags + ", START_STICKY=" + START_STICKY);
+        flags = START_STICKY;
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        L.i("DeamonService.onDestroy");
+        super.onDestroy();
+    }
+
+    public static class InnerService extends Service {
+        public IBinder onBind(Intent paramIntent) {
+            return null;
+        }
+
+        public void onCreate() {
+            super.onCreate();
+            L.i("DeamonService$InnerService.onCreate");
+
+            startForeground(64324, new Notification());
+            stopSelf();
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            L.i("DeamonService$InnerService.onStartCommand");
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        public void onDestroy() {
+            L.i("DeamonService$InnerService.onDestroy");
+            stopForeground(true);
+            super.onDestroy();
+        }
+    }
+}
---
+++ app/src/main/java/com/ruibin/deamonservice/L.java
@@ -0,0 +1,10 @@
+package com.ruibin.deamonservice;
+
+import android.util.Log;
+
+public class L {
+    private static final String TAG = "DeamonService";
+    public static void i(String msg) {
+        Log.i(TAG, msg);
+    }
+}
